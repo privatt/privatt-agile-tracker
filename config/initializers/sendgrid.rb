a77c9461516fe9cbb6fe206bbb203c2604a6afb5
Jimmy Beaudoin <email@jimmy-beaudoin.com>
2016-10-10 12:42:54 -0400

    Update sendgrid config files
    Add port and reorganize settings

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,11 +1,11 @@
-# Configure Heroku to use SendGrid.
 if ENV['SENDGRID_USERNAME']
   ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "25",
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN']
+    :address              => "smtp.sendgrid.net",
+    :authentication       => :plain,
+    :enable_starttls_auto => true,
+    :domain               => ENV['SENDGRID_DOMAIN'],
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :port                 => ENV['SENDGRID_PORT']
   }
 end
