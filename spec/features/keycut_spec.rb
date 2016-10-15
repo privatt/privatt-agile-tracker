976578ecb41ce78712a73f952c99ed23b7cbb36c
Jimmy Beaudoin <email@jimmy-beaudoin.com>
2016-10-15 13:06:56 -0400

    Update default wait time for Capybara in keycut spec

--- spec/features/keycut_spec.rb
+++ spec/features/keycut_spec.rb
@@ -5,6 +5,7 @@ describe "Keycuts" do
   self.use_transactional_fixtures = false
 
   before(:each) do
+    Capybara.default_max_wait_time = 5
     ActionController::Base.allow_forgery_protection = false
     DatabaseCleaner.clean
     sign_in user
